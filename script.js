efe79028fbb87f3587e323d08eb0b7c4326149a5
unknown <masumaakter.a1@gmail.com>
2024-02-18 21:17:11 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function myParagraph() {
+    document.getElementById("Paragraph").innerHTML="My Last Paragraph";
+   
+
+}
+
+
+function mywork() {
+    document.getElementById("Hey Girl").innerHTML="Hey Boy";
+    document.getElementById("Hey Girl").style.color="red";
+
+}
+
+
+
